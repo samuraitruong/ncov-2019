6f551cd158fcaf2572590d2b51622deeeb23d9d5
= <=>
2020-02-02 17:06:22 +1100

    added travis

--- .travis/push.sh
+++ .travis/push.sh
@@ -20,7 +20,8 @@ push_changes() {
   echo "---------------------GIT PUSH----------------------------------------------"
   git remote add tv https://${GH_TOKEN}@github.com/samuraitruong/ncov-2019.git>/dev/null 2>&1
   git log -3
-  git push --set-upstream tv master
+  git removte -v
+  git push tv master
   echo "----------------------------- VERIFY ---------------------------------------"
   git status
 }
