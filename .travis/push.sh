92df0fcc0b88c1660c926542fc668af398372187
= <=>
2020-02-02 16:48:45 +1100

    added travis

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,14 +9,17 @@ commit_website_files() {
   git status
   git add --all
   git commit -a --message "Travis build: $TRAVIS_BUILD_NUMBER [no CI]"
-
+  echo "---------------------------------------------------------"
   git status
   
 }
 
 push_changes() {
+  echo "---------------------GIT PUSH----------------------------------------------"
   git remote add tv https://${GH_TOKEN}@github.com/samuraitruong/ncov-2019.git>/dev/null 2>&1
-  git push --quiet --set-upstream tv master
+  git push --set-upstream tv master
+  echo "----------------------------- VERIY ---------------------------------------"
+  git status
 }
 
 setup_git
